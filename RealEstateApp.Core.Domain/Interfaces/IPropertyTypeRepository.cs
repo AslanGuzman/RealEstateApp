6f2a598486fb99cc994a7456be63d62c0cf6a8e8
Luis Santos <info.luisdev@gmail.com>
2026-07-11 16:33:45 -0400

    feat(domain): add repository interfaces

---
+++ RealEstateApp.Core.Domain/Interfaces/IPropertyTypeRepository.cs
@@ -0,0 +1,8 @@
+using RealEstateApp.Core.Domain.Entities;
+
+namespace RealEstateApp.Core.Domain.Interfaces
+{
+    public interface IPropertyTypeRepository : IGenericRepository<PropertyType>
+    {
+    }
+}
