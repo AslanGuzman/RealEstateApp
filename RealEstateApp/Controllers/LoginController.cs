cb854e4c6a770b610eda8b443ddd6d7e8b6dd561
Luis Santos <info.luisdev@gmail.com>
2026-07-14 00:07:05 -0400

    feat(web): add role menus and public logout redirect

--- RealEstateApp/Controllers/LoginController.cs
+++ RealEstateApp/Controllers/LoginController.cs
@@ -66,7 +66,7 @@ namespace RealEstateApp.Controllers
         public async Task<IActionResult> Logout()
         {
             await _accountService.SignOutAsync();
-            return RedirectToAction("Index", "Login");
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult AccessDenied()
