e3ec079e0d4aa9960e222d99a8571d55780f77bb
Luis Santos <info.luisdev@gmail.com>
2026-07-21 20:32:07 -0400

    refactor(application): annotate login request dto

--- Application/Dtos/User/LoginDto.cs
+++ Application/Dtos/User/LoginDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RealEstateApp.Core.Application.Dtos.User
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "Debe ingresar el usuario o correo electrónico")]
         public required string UserName { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar la contraseña")]
         public required string Password { get; set; }
     }
 }
